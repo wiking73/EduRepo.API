3dd27cda7f42ed1b15765aad4a6681891adb2063
wiktoria <pawlikowskaw@op.pl>
2025-04-11 11:41:37 +0200

    p

--- EduRepo.API/Program.cs
+++ EduRepo.API/Program.cs
@@ -5,10 +5,8 @@ using EduRepo.API.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddIdentityServices(builder.Configuration);
