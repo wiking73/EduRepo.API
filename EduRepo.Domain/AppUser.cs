c682a7ba3c01184eeefc5d77badcf8a0eeb4c2d1
wi <pawlikowskaw@op.pl>
2025-04-14 11:44:30 +0200

    User

---
+++ EduRepo.Domain/AppUser.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+namespace EduRepo.Domain
+{
+    public class AppUSer : IdentityUser
+    {
+        [Key]
+        
+        public string UserName { get; set; }
+        public string Haslo { get; set; }
+       // public string Imie { get; set; }
+        public string Email { get; set; }
+      //  public string Rola { get; set; }
+      //  public string Klasa { get; set; }
+        
+      //  public ICollection<Kurs> KursyWlasne { get; set; }
+      ////  public ICollection<Uczestnictwo> Uczestnictwa { get; set; }
+      //  public ICollection<Odpowiedz> Odpowiedzi { get; set; }
+       // public ICollection<PowiadomienieBrakOdpowiedzi> Powiadomienia { get; set; }
+
+
+    }
+
+   /* public class Uczestnictwo
+    {
+        [Key]
+        public int IdKursu { get; set; }
+        public Kurs Kurs { get; set; }
+
+        public int IdUzytkownika { get; set; }
+        public Uzytkownik Uzytkownik { get; set; }
+
+        public string Status { get; set; }
+    }*/
+
+}
